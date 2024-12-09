c80057c2985c3720a21d5f39c2bc96f85c733cbe
Yeonnies <eliyeon16@gmail.com>
2024-12-10 00:14:14 +0900

    8주차-인터페이스로 수정

--- src/main/java/umc/spring/repository/FoodCategoryRepository/FoodCategoryRepository.java
+++ src/main/java/umc/spring/repository/FoodCategoryRepository/FoodCategoryRepository.java
@@ -3,5 +3,5 @@ package umc.spring.repository.FoodCategoryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.spring.domain.FoodCategory;
 
-public class FoodCategoryRepository extends JpaRepository<FoodCategory, Long> {
+public interface FoodCategoryRepository extends JpaRepository<FoodCategory, Long> {
 }
